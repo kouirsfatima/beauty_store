2a9264a927ed471aad2433f803c894e09b283e2a
kouirsfatima <fatimaberline535@gmail.com>
2024-07-10 21:02:17 +0100

    add welcome part

--- Blackclover/src/App.js
+++ Blackclover/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import NavBar from './components/Navbar/NavBar'
 import HomeBanner from './components/banner/HomeBanner'
 import Footer from './components/Footer/Footer'
-import Header from './components/header/Header'
+import Products from './components/Product/Products'
+import Welcome from './components/Welcome/Welcome'
 
 const App = () => {
   return (
     <div className='container'>
       <NavBar/>
       <HomeBanner/>
-      <Header/>
+      <Welcome/>
+      <Products/>
       <Footer/>
     </div>
   )
